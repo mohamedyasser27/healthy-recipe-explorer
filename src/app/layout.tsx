911256fe8e7752ccdc6b9ec62d07b44d6cc72209
mohamed yasser <mohamed.yasser2725@gmail.com>
2025-10-18 04:46:05 +0300

    feat: add shadcn with custom theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
 import "./globals.css";
 
+import { Courier_Prime } from "next/font/google";
+
+const courierPrime = Courier_Prime({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-courier-prime",
+});
+
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={`${courierPrime.variable}`}>
 			<body>{children}</body>
 		</html>
 	);
